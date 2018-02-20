641e552c3283a8ea0c5ea1811d5c941016ec0638
Philipp Brüll <pb@simia.tech>
2018-02-20 14:44:29 +0100

    added cargo setup

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -1,4 +1,5 @@
 global start
+extern long_mode_start
 
 section .bss
 align 4096
@@ -12,6 +13,15 @@ stack_bottom:
   resb 64
 stack_top:
 
+section .rodata
+gdt64:
+  dq 0 ; zero entry
+.code: equ $ - gdt64
+  dq (1<<43) | (1<<44) | (1<<47) | (1<<53) ; code segment
+.pointer:
+  dw $ - gdt64 - 1
+  dq gdt64
+
 section .text
 bits 32
 start:
@@ -24,9 +34,10 @@ start:
   call set_up_page_tables
   call enable_paging
 
-  ; print 'OK' to screen
-  mov dword [0xb8000], 0x2f4b2f4f
-  hlt
+  ; load the 64-bit GDT
+  lgdt [gdt64.pointer]
+
+  jmp gdt64.code:long_mode_start
 
 check_multiboot:
   cmp eax, 0x36d76289
